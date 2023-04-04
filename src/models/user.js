53c4404efb2066fd25b5b100e71116c1e03322e0
aliusuf <37791042+aliusuf@users.noreply.github.com>
2023-04-04 13:48:01 +0500

    Updated version

--- src/models/user.js
+++ src/models/user.js
@@ -13,7 +13,7 @@ const userSchema = new Schema({
     trim: true,
     unique: true,
     match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+      /^[a-zA-Z0-9._%+-]+@(gmail\.com|googlemail\.com)$/,
       "Please fill a valid email address",
     ],
   },
